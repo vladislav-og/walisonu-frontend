3aabf866b70dd450bf47e8561b5a18973a8eed42
Vladislav <Vladislav.Ogorodnik@ttu.ee>
2019-10-10 12:17:55 +0300

    Add basic webpack config and remove unnecessary files.

---
+++ frontend/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: "./src/index.js",
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: "babel-loader",
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(png|svg|jpg|gif|ico)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    }
+                ],
+
+            },
+        ]
+    },
+    resolve: { extensions: ["*", ".js", ".jsx"] },
+    output: {
+        path: path.join(__dirname, "/build"),
+        filename: "[name].[hash].bundle.js"
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html"
+        }),
+        new CopyWebpackPlugin([
+            { from: './public/favicon.ico'},
+            {from: './public/manifest.json'}, // <- your path to favicon
+        ])]
+};
+

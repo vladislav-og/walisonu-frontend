10076fb1a3e82855a62675d2788bbf381125591f
Kauri Riivik <kauririivik@gmail.com>
2019-10-26 21:31:47 +0300

    Add logo to navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,18 +1,28 @@
-import React, {Component} from "react";
-import 'bootstrap/dist/css/bootstrap.css';
-import {Link} from 'react-router-dom';
-import {Navbar, Nav } from "react-bootstrap";
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import { Link } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
 
 //import * as requests from '../utils/requests';
 
 class NavbarComp extends Component {
+  render() {
+    return (
+      <Navbar bg="primary" variant="dark">
+        <Link to="/">
+          <Navbar.Brand>
+            <img
+              src="/src/static/images/favicon-side.png"
+              width="auto"
+              height="40"
+              className="d-inline-block align-top"
+              alt="Wali Sõnu!"
+            />
+          </Navbar.Brand>
+        </Link>
+      </Navbar>
+    );
+  }
+}
 
-    render() {
-        return (
-            <Navbar bg="primary" variant="dark">
-                <Link to="/"><Navbar.Brand>Wali sõnu!</Navbar.Brand></Link>
-            </Navbar>
-        )
-}}
-
-export default (NavbarComp);
+export default NavbarComp;

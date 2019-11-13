b484319e97843ad553727598fb531f774cf1277e
Kauri Riivik <kauririivik@gmail.com>
2019-11-13 14:30:40 +0200

    Repair not secure proxy

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,51 +1,54 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    entry: "./src/index.js",
-    mode: "development",
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: "babel-loader",
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            },
-            {
-                test: /\.(png|svg|jpg|gif|ico)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                    }
-                ],
-
-            },
-        ]
-    },
-    resolve: { extensions: ["*", ".js", ".jsx"] },
-    output: {
-        path: path.join(__dirname, "/build"),
-        filename: "[name].[hash].bundle.js"
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./public/index.html"
-        }),
-        new CopyWebpackPlugin([
-            { from: './public/favicon.ico'},
-            { from: './public/favicon-side.png'},
-            {from: './public/manifest.json'}, // <- your path to favicon
-        ])],
-    devServer: {
-        http2: true,
-        proxy: {
-            '/api': 'https://174.129.108.32'
+  entry: "./src/index.js",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif|ico)$/,
+        use: [
+          {
+            loader: "file-loader"
           }
+        ]
+      }
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.join(__dirname, "/build"),
+    filename: "[name].[hash].bundle.js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    }),
+    new CopyWebpackPlugin([
+      { from: "./public/favicon.ico" },
+      { from: "./public/favicon-side.png" },
+      { from: "./public/manifest.json" } // <- your path to favicon
+    ])
+  ],
+  devServer: {
+    http2: true,
+    https: true,
+    proxy: { 
+        "/api/**": { 
+            target: "https://174.129.108.32", 
+            secure: false 
+        } 
     }
+  }
 };
-

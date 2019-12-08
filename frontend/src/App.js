a15160faa3547a0aabe7b0e962df12f160a06375
Vladislav <Vladislav.Ogorodnik@ttu.ee>
2019-12-08 16:29:58 +0200

    Add register page react component without functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Route, HashRouter as Router, Switch} from 'react-router-dom';
 import WordsList from './components/WordsList'
 import LoginPage from './components/LoginPage'
+import RegisterPage from './components/RegisterPage'
 import NavbarComp from './components/Navbar'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
                   <Switch>
                       <Route exact path="/" component={WordsList}/>
                       <Route exact path="/login" component={LoginPage}/>
+                      <Route exact path="/register" component={RegisterPage}/>
                   </Switch>
               </div>
           </Router>
---
+++ frontend/src/components/RegisterPage.js
@@ -0,0 +1,105 @@
+import React, {Component} from 'react';
+import '../static/css/pages.css'
+import {Button, Form} from "react-bootstrap";
+import {Link} from "react-router-dom";
+import PropTypes from "prop-types";
+import {Redirect} from "react-router-dom";
+
+class RegisterPage extends Component {
+    static propTypes = {
+        register: PropTypes.func.isRequired,
+        isAuthenticated: PropTypes.bool,
+    };
+
+    state = {
+        username: "",
+        email: "",
+        password1: "",
+        password2: "",
+    };
+
+    onSubmit = e => {
+        e.preventDefault();
+        const {username, email, password1, password2} = this.state;
+
+        if(password1 !== password2) {
+            this.props.createMessage({passwordNotMatch: "Passwords do not match"});
+        } else {
+            const newUser = {
+                username,
+                password1,
+                password2,
+                email,
+            };
+            this.props.register(newUser)
+        }
+    };
+
+    onChange = e => {
+        this.setState({[e.target.name]: e.target.value});
+    };
+
+
+    render() {
+        if(this.props.isAuthenticated) {
+            return <Redirect to="/" />
+        }
+        const { username, email, password, password2 } = this.state;
+        return (
+            <div className="form">
+                <Form onSubmit={this.onSubmit}>
+                    <Form.Group>
+                        <Form.Label >Username</Form.Label>
+                        <Form.Control type="text"
+                                      className="form-input"
+                                      name="username"
+                                      onChange={this.onChange}
+                                      placeholder="Username"
+                                      value={username}
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label >Email</Form.Label>
+                        <Form.Control type="email"
+                                      className="form-input"
+                                      name="email"
+                                      onChange={this.onChange}
+                                      placeholder="Email"
+                                      value={email}
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Password 1</Form.Label>
+                        <Form.Control type="password"
+                                      className="form-input"
+                                      name="password1"
+                                      onChange={this.onChange}
+                                      placeholder="Password"
+                                      value={password}
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Password 2</Form.Label>
+                        <Form.Control type="password"
+                                      className="form-input"
+                                      name="password2"
+                                      onChange={this.onChange}
+                                      placeholder="Repeat Password"
+                                      value={password2}
+                        />
+                    </Form.Group>
+                    <div className="form-group">
+                        <Button type="submit" block>
+                            Register
+                        </Button>
+                    </div>
+                    <p>
+                        Already have account? <Link to="/login">Login</Link>
+                    </p>
+                </Form>
+            </div>
+        );
+    }
+}
+
+export default (RegisterPage);

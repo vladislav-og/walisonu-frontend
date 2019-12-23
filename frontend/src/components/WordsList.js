b7c7adc8c76c1da82b0d36d03c49fc68c95c2fd7
Vladislav <Vladislav.Ogorodnik@ttu.ee>
2019-12-23 15:19:16 +0200

    Remove prints

--- frontend/src/components/WordsList.js
+++ frontend/src/components/WordsList.js
@@ -9,7 +9,6 @@ class WordsList extends Component {
 
     constructor(props) {
         super(props);
-        console.log(this.props);
         this.state = {
             words: [],
             inputWord: '',
